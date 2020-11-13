9ff593aa3ce8987923e2f97641ff3f1db0aa6cc0
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2020-11-14 01:06:19 +0500

    removed unnecessary code

--- src/Components/Bootstrap.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Bootstrap() {
-  return (
-    <div>
-      Hello from Bootstrap
-    </div>
-  )
-}
--- src/Components/Budget.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Budget() {
-  return (
-    <div>
-      Hello from Budget
-    </div>
-  )
-}
