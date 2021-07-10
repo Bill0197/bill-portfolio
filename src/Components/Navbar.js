dd77c2a04815ea73b144a0ceb53a79fee4a9e825
Khabibullo Saydullaev <khabibullosaydullaev@gmail.com>
2021-07-11 03:29:52 +0500

    updated cv

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,97 +1,135 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { NavIcon, Wrapper } from "../Styles/Styles";
+import React, { useState, useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
+import { NavIcon, Wrapper } from '../Styles/Styles'
 
-export default function Navbar({ offset, forwardRef }) {
-    const [state, setState] = useState(false);
-    const [show, setShow] = useState(false);
+export default function Navbar({ offset }) {
+  const [state, setState] = useState(false)
+  const [show, setShow] = useState(false)
+  const [windowSize, setWindowSize] = useState(window.innerWidth)
 
-    const handleClick = ({ target }) => {
-        let icon = document.getElementById("toggleIcon");
-        setTimeout(() => {
-            if (target === icon || icon.hasChildNodes) {
-                setShow(!show);
-                clearTimeout();
-            } else {
-                setShow(true);
-            }
-        }, 120);
-    };
+  const handleClick = ({ target }) => {
+    let icon = document.getElementById('toggleIcon')
+    setTimeout(() => {
+      if (target === icon || icon.hasChildNodes) {
+        setShow(!show)
 
-    window.onscroll = function () {
-        myFunction();
-    };
+        clearTimeout()
+      } else {
+        setShow(true)
+      }
+    }, 120)
+  }
 
-    function myFunction() {
-        if (window.pageYOffset > offset) {
-            setState(true);
-        } else {
-            setState(false);
-        }
+  window.onscroll = function () {
+    myFunction()
+  }
+
+  function myFunction() {
+    if (window.pageYOffset > offset) {
+      setState(true)
+    } else {
+      setState(false)
     }
-    useEffect(() => {
-        const reff = document.getElementById("inputref");
-        const toggle = document.querySelector(".toggle");
-        let inputRef = document.querySelector("#inputref");
-        inputRef.addEventListener("click", handleClick);
-        reff.onclick = () => {
-            if (reff.hasChildNodes(toggle)) {
-                if (show) {
-                    setShow(!show);
-                }
-            } else {
-                if (!show) {
-                    setShow(show);
-                }
-            }
-        };
-    }, []);
-    return (
-        <nav className={state ? "navbarArea navbarFixed" : "navbarArea"}>
-            <Link to="/">
-                <li className="navbar-brand">Saydullaev</li>
-            </Link>
-            <ul
-                className="navbarNav"
-                style={{
-                    transition: "display .3s linear",
-                    display: show && window.innerWidth < 930 ? "none" : "flex",
-                }}
-                ref={(node) => {
-                    node = node;
-                }}
-            >
-                <li className="navItem">
-                    <Link className="navItem" to="/">
-                        Home
-                    </Link>
-                </li>
-                <li className="navItem">
-                    <Link className="navItem" to="/about">
-                        ABOUT
-                    </Link>
-                </li>
-                <li className="navItem">
-                    <Link className="navItem" to="/services">
-                        SERVICES
-                    </Link>
-                </li>
-                <li className="navItem" id="pagesHover">
-                    <Link className="navItem" to="/portfolio">
-                        PORTFOLIO
-                    </Link>
-                </li>
-                <li className="navItem">
-                    <Link className="navItem" to="/contact">
-                        CONTACT
-                    </Link>
-                </li>
-            </ul>
-            <div className="toggle">
-                <Wrapper>
-                    <NavIcon onClick={handleClick} id="toggleIcon" />
-                </Wrapper>
-            </div>
-        </nav>
-    );
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWindowSize(window.innerWidth))
+    windowSize > 930 && setShow(false)
+
+    return () =>
+      window.removeEventListener('resize', () =>
+        setWindowSize(window.innerWidth)
+      )
+  }, [windowSize])
+
+  useEffect(() => {
+    setShow(window.innerWidth > 930 ? false : true)
+
+    const toggle = document.querySelector('.toggle')
+    const inputRef = document.querySelector('#inputref')
+
+    inputRef.addEventListener('click', handleClick)
+    inputRef.onclick = () => {
+      if (inputRef.hasChildNodes(toggle)) {
+        if (show) {
+          setShow(!show)
+        }
+      } else {
+        if (!show) {
+          setShow(show)
+        }
+      }
+    }
+  }, [])
+
+  const activeStyles = {
+    color: '#4a5cfa',
+  }
+
+  return (
+    <nav className={state ? 'navbarArea navbarFixed' : 'navbarArea'}>
+      <NavLink exact activeStyle={activeStyles} to="/">
+        <li className="navbar-brand">Saydullaev</li>
+      </NavLink>
+      <ul
+        className="navbarNav"
+        style={{
+          transition: 'display .3s linear',
+          display: show && window.innerWidth < 930 ? 'none' : 'flex',
+        }}
+        ref={node => {
+          node = node
+        }}
+      >
+        <li className="navItem">
+          <NavLink exact activeStyle={activeStyles} className="navItem" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink
+            exact
+            activeStyle={activeStyles}
+            className="navItem"
+            to="/about"
+          >
+            ABOUT
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink
+            activeStyle={activeStyles}
+            className="navItem"
+            to="/services"
+          >
+            SERVICES
+          </NavLink>
+        </li>
+        <li className="navItem" id="pagesHover">
+          <NavLink
+            activeStyle={activeStyles}
+            className="navItem"
+            to="/portfolio"
+          >
+            PORTFOLIO
+          </NavLink>
+        </li>
+        <li className="navItem">
+          <NavLink
+            exact
+            activeStyle={activeStyles}
+            className="navItem"
+            to="/contact"
+          >
+            CONTACT
+          </NavLink>
+        </li>
+      </ul>
+      <div className="toggle">
+        <Wrapper>
+          <NavIcon onClick={handleClick} id="toggleIcon" />
+        </Wrapper>
+      </div>
+    </nav>
+  )
 }
--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -1,143 +1,162 @@
-import React from "react";
-import GenericHeader from "./GenericHeader";
-import Footer from "./Footer";
-import Hotel from "../assets/portfolio/hotel.PNG";
-import Webbrain from "../assets/portfolio/webbrain.PNG";
-import Bootstrap from "../assets/portfolio/bootstrap.PNG";
-import Budget from "../assets/portfolio/budget.PNG";
-import { Link } from "react-router-dom";
+import React from 'react'
+import GenericHeader from './GenericHeader'
+import Footer from './Footer'
+import Hotel from '../assets/portfolio/hotel.PNG'
+import Webbrain from '../assets/portfolio/webbrain.PNG'
+import Bootstrap from '../assets/portfolio/bootstrap.PNG'
+import Budget from '../assets/portfolio/budget.PNG'
+import { Link } from 'react-router-dom'
+
+const links = [
+  { link: 'https://loadfetcher.com/', name: 'loadfetcher' },
+  { link: 'https://rapidboard-q3tidrazma-uk.a.run.app/', name: 'rapidboard' },
+  { link: 'https://wbba.netlify.app/', name: 'web brain design' },
+  { link: 'https://bill-hotel-site.netlify.app/', name: 'hotel rooms' },
+  {
+    link: 'https://dispatch-report-maker.netlify.app/',
+    name: 'driver expenses report maker',
+  },
+  {
+    link: 'https://chat-app-socket-io.netlify.app/',
+    name: 'simple socket chat app',
+  },
+]
 
 export default function Portfolio({ display }) {
-    return (
-        <>
-            {!display && <GenericHeader title="PORTFOLIO" page="Portfolio " />}
-            <section id="portfolio" className="center">
-                <div className="portfolio">
-                    <h3>MY RECENT COMPLETED PROJECTS</h3>
-                    <p>
-                        These are just some projects you are seeing, I have way
-                        more projects I had practiced.
-                    </p>
-                    <p>1. Webbrain</p>
-                    <p>2. Hotel Reservation </p>
-                    <p>3. Bootstrap Portfolio </p>
-                    <p>4. Budget App </p>
-                </div>
-                <div className="imgWrapper">
-                    <div className="imgParent">
-                        <a
-                            className="displayWebsite portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://wbba.netlify.app/"
-                        >
-                            App Website
-                        </a>
-                        <a className="displayCode portfolioBtn biggerFont">
-                            non public &#x21;
-                        </a>
-                        <Link
-                            to="/portfolio/webbrain"
-                            className="portfolioBtn displayDetails biggerFont"
-                        >
-                            Project Details
-                        </Link>
-                        <img
-                            className="imgPortf"
-                            src={Webbrain}
-                            alt="hotel reservation site"
-                        />
-                    </div>
-                    <div className="imgParent">
-                        <a
-                            className="displayWebsite portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://bill-hotel-site.netlify.app/"
-                        >
-                            App Website
-                        </a>
-                        <a
-                            className="displayCode portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://github.com/Bill0197/hotel-reservation"
-                        >
-                            Source Code
-                        </a>
-                        <Link
-                            to="/portfolio/hotel"
-                            className="portfolioBtn displayDetails biggerFont"
-                        >
-                            Project Details
-                        </Link>
-                        <img
-                            className="imgPortf"
-                            src={Hotel}
-                            alt="hotel reservation site"
-                        />
-                    </div>
-                    <div className="imgParent">
-                        <a
-                            className="displayWebsite portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://khabibullosaydullaev.netlify.app/"
-                        >
-                            App Website
-                        </a>
-                        <a
-                            className="displayCode portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://github.com/Bill0197/bootstrap-the-updated-one"
-                        >
-                            Source Code
-                        </a>
-                        <Link
-                            to="/portfolio/bootstrap"
-                            className="portfolioBtn displayDetails biggerFont"
-                        >
-                            Project Details
-                        </Link>
-                        <img
-                            className="imgPortf"
-                            src={Bootstrap}
-                            alt="portfolio site with bootstrap"
-                        />
-                    </div>
-                    <div className="imgParent">
-                        <a
-                            className="displayWebsite portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://bill-budget.netlify.app/"
-                        >
-                            App Website
-                        </a>
-                        <a
-                            className="displayCode portfolioBtn biggerFont"
-                            target="blank"
-                            href="https://github.com/Bill0197/bill-budget"
-                        >
-                            Source Code
-                        </a>
-                        <Link
-                            to="/portfolio/budget"
-                            className="portfolioBtn displayDetails biggerFont"
-                        >
-                            Project Details
-                        </Link>
-                        <img
-                            className="imgPortf"
-                            src={Budget}
-                            alt="budget site"
-                        />
-                    </div>
-                </div>
-            </section>
-            {!display && (
-                <div className="fullWidth">
-                    <Link to="/" className="center bannerBtn">
-                        back to home
-                    </Link>
-                </div>
-            )}
-            {!display && <Footer />}
-        </>
-    );
+  return (
+    <>
+      {!display && <GenericHeader title="PORTFOLIO" page="Portfolio " />}
+      <section id="portfolio" className="center">
+        <div className="portfolio">
+          <h3>MY RECENT COMPLETED PROJECTS</h3>
+          {links.map(l => (
+            <div style={{ marginBottom: '10px' }} key={l.link}>
+              <a href={l.link} target="_blank" rel="noopener noreferrer">
+                {l.name}
+              </a>
+            </div>
+          ))}
+
+          <p>
+            These are just some projects you are seeing, I have way more
+            projects I had practiced.
+          </p>
+          <p>1. Webbrain</p>
+          <p>2. Hotel Reservation </p>
+          <p>3. Bootstrap Portfolio </p>
+          <p>4. Budget App </p>
+        </div>
+        <div className="imgWrapper">
+          <div className="imgParent">
+            <a
+              className="displayWebsite portfolioBtn biggerFont"
+              target="blank"
+              href="https://wbba.netlify.app/"
+            >
+              App Website
+            </a>
+            <a className="displayCode portfolioBtn biggerFont">
+              non public &#x21;
+            </a>
+            <Link
+              to="/portfolio/webbrain"
+              className="portfolioBtn displayDetails biggerFont"
+            >
+              Project Details
+            </Link>
+            <img
+              className="imgPortf"
+              src={Webbrain}
+              alt="hotel reservation site"
+            />
+          </div>
+          <div className="imgParent">
+            <a
+              className="displayWebsite portfolioBtn biggerFont"
+              target="blank"
+              href="https://bill-hotel-site.netlify.app/"
+            >
+              App Website
+            </a>
+            <a
+              className="displayCode portfolioBtn biggerFont"
+              target="blank"
+              href="https://github.com/Bill0197/hotel-reservation"
+            >
+              Source Code
+            </a>
+            <Link
+              to="/portfolio/hotel"
+              className="portfolioBtn displayDetails biggerFont"
+            >
+              Project Details
+            </Link>
+            <img
+              className="imgPortf"
+              src={Hotel}
+              alt="hotel reservation site"
+            />
+          </div>
+          <div className="imgParent">
+            <a
+              className="displayWebsite portfolioBtn biggerFont"
+              target="blank"
+              href="https://khabibullosaydullaev.netlify.app/"
+            >
+              App Website
+            </a>
+            <a
+              className="displayCode portfolioBtn biggerFont"
+              target="blank"
+              href="https://github.com/Bill0197/bootstrap-the-updated-one"
+            >
+              Source Code
+            </a>
+            <Link
+              to="/portfolio/bootstrap"
+              className="portfolioBtn displayDetails biggerFont"
+            >
+              Project Details
+            </Link>
+            <img
+              className="imgPortf"
+              src={Bootstrap}
+              alt="portfolio site with bootstrap"
+            />
+          </div>
+          <div className="imgParent">
+            <a
+              className="displayWebsite portfolioBtn biggerFont"
+              target="blank"
+              href="https://bill-budget.netlify.app/"
+            >
+              App Website
+            </a>
+            <a
+              className="displayCode portfolioBtn biggerFont"
+              target="blank"
+              href="https://github.com/Bill0197/bill-budget"
+            >
+              Source Code
+            </a>
+            <Link
+              to="/portfolio/budget"
+              className="portfolioBtn displayDetails biggerFont"
+            >
+              Project Details
+            </Link>
+            <img className="imgPortf" src={Budget} alt="budget site" />
+          </div>
+        </div>
+      </section>
+      {!display && (
+        <div className="fullWidth">
+          <Link to="/" className="center bannerBtn">
+            back to home
+          </Link>
+        </div>
+      )}
+      {!display && <Footer />}
+    </>
+  )
 }
